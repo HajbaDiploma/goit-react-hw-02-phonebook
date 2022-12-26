519997ff6f090bec16b9dd450baccf29410c0371
Volodymyr Khaiba <hajba.vova@gmail.com>
2022-12-26 20:46:58 +0200

    init

---
+++ src/components/Filter/Filter.styled.jsx
@@ -0,0 +1,12 @@
+import styled from "@emotion/styled";
+
+export const Input = styled.input`
+width: 300px;
+height: 30px;
+margin-left: 10px;
+font-size: 24px;c
+`
+
+export const Label = styled.label`
+  font-weight: bold;
+`
---
+++ src/constants/theme.js
@@ -0,0 +1,9 @@
+export const theme = {
+  colors: {
+    mainButtonColor: 'lavenderblush',
+    mainBgColor: 'white',
+    secondBgColor: 'gray',
+    titleColor: 'steelblue',
+  },
+  spacing: (value) => `${value*4}px`,
+}
